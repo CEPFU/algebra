3637ccf292871131418bb5d7c78c2c63271e86f9
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-07-29 11:09:00 +0200

    Use Hamcrest matchers in tests

--- src/test/java/de/fu_berlin/agdb/crepe/algebra/operators/logic/BinaryOperatorTypeTest.java
+++ src/test/java/de/fu_berlin/agdb/crepe/algebra/operators/logic/BinaryOperatorTypeTest.java
@@ -5,9 +5,8 @@ import org.junit.Test;
 import java.util.function.BinaryOperator;
 
 import static de.fu_berlin.agdb.crepe.algebra.operators.logic.BinaryOperatorType.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Test boolean binary operator types.
@@ -19,35 +18,35 @@ public class BinaryOperatorTypeTest {
     @Test
     public void testAndOperator() throws Exception {
         BinaryOperator<Boolean> and = AND.getOperator();
-        assertFalse("false && false should return false", and.apply(false, false));
-        assertFalse("false && true should return false", and.apply(false, true));
-        assertFalse("true && false should return false", and.apply(true, false));
-        assertTrue("true && true should return true", and.apply(true, true));
+        assertThat("false && false should return false", and.apply(false, false), is(false));
+        assertThat("false && true should return false", and.apply(false, true), is(false));
+        assertThat("true && false should return false", and.apply(true, false), is(false));
+        assertThat("true && true should return true", and.apply(true, true), is(true));
     }
 
     @Test
     public void testOrOperator() throws Exception {
         BinaryOperator<Boolean> or = OR.getOperator();
-        assertFalse("false && false should return false", or.apply(false, false));
-        assertTrue("false && true should return true", or.apply(false, true));
-        assertTrue("true && false should return true", or.apply(true, false));
-        assertTrue("true && true should return true", or.apply(true, true));
+        assertThat("false && false should return false", or.apply(false, false), is(false));
+        assertThat("false && true should return true", or.apply(false, true), is(true));
+        assertThat("true && false should return true", or.apply(true, false), is(true));
+        assertThat("true && true should return true", or.apply(true, true), is(true));
     }
 
     @Test
     public void testXorOperator() throws Exception {
         BinaryOperator<Boolean> xor = XOR.getOperator();
-        assertFalse("false && false should return false", xor.apply(false, false));
-        assertTrue("false && true should return true", xor.apply(false, true));
-        assertTrue("true && false should return true", xor.apply(true, false));
-        assertFalse("true && true should return false", xor.apply(true, true));
+        assertThat("false && false should return false", xor.apply(false, false), is(false));
+        assertThat("false && true should return true", xor.apply(false, true), is(true));
+        assertThat("true && false should return true", xor.apply(true, false), is(true));
+        assertThat("true && true should return false", xor.apply(true, true), is(false));
     }
 
 
     @Test
     public void testGetUnit() throws Exception {
-        assertEquals("Unit for 'AND' is incorrect.", true, AND.getUnit());
-        assertEquals("Unit for 'OR' is incorrect.", false, OR.getUnit());
-        assertEquals("Unit for 'XOR' is incorrect.", false, XOR.getUnit());
+        assertThat("Unit for 'AND' is incorrect.", AND.getUnit(), is(true));
+        assertThat("Unit for 'OR' is incorrect.", OR.getUnit(), is(false));
+        assertThat("Unit for 'XOR' is incorrect.", XOR.getUnit(), is(false));
     }
 }
